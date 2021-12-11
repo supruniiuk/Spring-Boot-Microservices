2837538eb4d46f0ce168d0fe7d259c2079d7a8c1
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-11 16:44:38 +0200

    created project

---
+++ src/main/java/com/searchjob/servicejob/ServiceJobApplication.java
@@ -0,0 +1,13 @@
+package com.searchjob.servicejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/searchjob/servicejob/ServiceJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchjob.servicejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
