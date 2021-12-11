e74d8901c8b26d23f3e41bd20517880eb7860cca
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-11 21:17:11 +0200

    created vacancy model

--- src/main/java/com/searchjob/servicejob/repository/VacancyRepository.java
+++ src/main/java/com/searchjob/servicejob/repository/VacancyRepository.java
@@ -1,2 +1,7 @@
-package com.searchjob.servicejob.repository;public interface VacancyRepository {
+package com.searchjob.servicejob.repository;
+import com.searchjob.servicejob.repository.model.Vacancy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//JpaRepository works with entities of class Vacancy with unique identifier
+public interface VacancyRepository extends JpaRepository<Vacancy, Long>{
 }
--- src/main/java/com/searchjob/servicejob/repository/model/Vacancy.java
+++ src/main/java/com/searchjob/servicejob/repository/model/Vacancy.java
@@ -1,2 +1,83 @@
-package com.searchjob.servicejob.repository.model;public class Vacancy {
+package com.searchjob.servicejob.repository.model;
+
+import lombok.Generated;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vacancies")
+public final class Vacancy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String typeOfEmployment;
+    private String responsibilities;
+    private String skills;
+    private String employer;
+    private String email;
+
+
+    public Vacancy(){
+
+    }
+
+    public Vacancy(String title, String typeOfEmployment,
+               String responsibilities, String skills,
+               String employer, String email){
+        this.title = title;
+        this.typeOfEmployment = typeOfEmployment;
+        this.responsibilities = responsibilities;
+        this.skills = skills;
+        this.employer = employer;
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTypeOfEmployment() {
+        return typeOfEmployment;
+    }
+
+    public void setTypeOfEmployment(String typeOfEmployment) {
+        this.typeOfEmployment = typeOfEmployment;
+    }
+
+    public String getResponsibilities() {
+        return responsibilities;
+    }
+
+    public void setResponsibilities(String responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
