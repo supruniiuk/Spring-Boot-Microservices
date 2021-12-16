4170a0dc5eb3331fda53ab3974eabe2d55072873
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-16 02:25:51 +0200

    created dockerfile for userservice

--- service-feedback/src/main/java/com/searchjob/servicefeedback/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.searchjob.servicefeedback.repository;
-
-import com.searchjob.servicefeedback.repository.model.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository  extends JpaRepository<Feedback, Long> {
-}
